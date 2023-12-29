9abb7bdacae9a3b1e429502159ba190166e5aacd
RazeenBhai <razeenbaig10@gmail.com>
2023-12-30 00:39:03 +0500

    deleted some files

--- backend/routes/admin.js
+++ backend/routes/admin.js
@@ -3,6 +3,17 @@ const router = Router();
 const adminMiddleware = require('../middleware/admin')
 const zod = require('zod');
 
+
+const username = zod.string();
+const password = zod.string().min(6);
+
 router.get('/signup', adminMiddleware, async (req, res) => {
+    const usernameResponse = req.headers.username;
+    const passwordResponse = req.headers.password;
+
+
+    const newAdmin = Admin.create({
+        username: 
+    })
     
 })
