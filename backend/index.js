76998ddfaad8b50e4478933fd87217d6f2911ec3
RazeenBhai <razeenbaig10@gmail.com>
2023-12-29 19:03:43 +0500

    started some backend

--- backend/index.js
+++ backend/index.js
@@ -1,33 +1,10 @@
-const Users = require('./db/user')
-require('./db/mongodb')
-const mongoose = require('mongoose')
+const zod = require('zod');
 const express = require('express');
-const app = express()
-const cors = require('cors');
-
-app.use(cors())
-
-let port = 8000;
+const jwt = require('jsonwebtoken')
+const app = express();
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send('Server is Started')
-})
+let username = zod.string();
+let password = zod.string().min(6);
 
-app.post('/register', async (req, res) => {
-    // let data =  await Users.find();
-    let data = await Users.collection.insertOne({
-        name: req.body.name,
-        email: req.body.email,
-        password: req.body.password,
-    })
-    let result = await data.save();
-    // let data = await Products.collection.deleteOne({name : req.params.name})
-    console.log(result);
-    res.send(result);
-})
-
-app.listen(port, ()=> {
-    console.log(`Server Listening at port ${port}`)
-})
