b20423515af86118980a36e8e137a7f21c5dab59
Razeen <razeenbaig10@gmail.com>
2024-01-21 02:56:36 +0500

    Update SecondSection.jsx

--- frontend/src/components/Home/SecondSection.jsx
+++ frontend/src/components/Home/SecondSection.jsx
@@ -5,7 +5,27 @@ const SecondSection = () => {
   return (
     <div className="section">
       <div className="container">
-        <div className="left"></div>
+        <div className="left">
+          <div className="top">
+            <h2>Unlock the Elegance of Rosé</h2>
+            <p>
+              Embrace the allure of Rosé and elevate your moments to a whole new
+              level of sophistication. Indulge in the exquisite taste of our
+              carefully crafted Rosé, a wine that embodies elegance and captures
+              the essence of fleeting, beautiful moments. Join the celebration
+              and order your bottle of Rosé today. Experience the magic in every
+              sip and toast to the extraordinary. Buy now and savor life's
+              pleasures, one glass at a time.
+            </p>
+          </div>
+          <div className="mid">
+            <h2>Rosé Premium Gold</h2>
+          </div>
+          <div className="bottom">
+            <button>Add to Cart</button>
+            <h2>$194.99</h2>
+          </div>
+        </div>
         <div className="right"></div>
       </div>
     </div>
