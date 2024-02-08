d82aa11a5fa6284807fcb9f54e574243d4d969bb
Razeen <razeenbaig10@gmail.com>
2024-02-08 22:44:46 +0500

    continuing footer

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import FirstSection from "./Home/FirstSection";
 import SecondSection from "./Home/SecondSection";
 import ThirdSection from "./Home/ThirdSection";
+import Footer from "./Home/Footer";
 const Home = () => {
   return (
     <div>
       <FirstSection />
       <SecondSection />
       <ThirdSection />
+      <Footer />
     </div>
   );
 };
---
+++ frontend/src/components/Home/Footer.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./Footer.css";
+const Footer = () => {
+  return (
+    <div className="footer">
+      <div className="left">
+        <h2>Rosé</h2>
+      </div>
+      <div className="mid">
+        <h3>All Rights Reserved</h3>
+      </div>
+      <div className="right">
+        
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
